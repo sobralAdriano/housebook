421c3e2fe137104920357d7515028554d8f6f65e
Michael Guilherme de Oliveira Nascimento <michael.oliveira@softplan.com.br>
2021-10-01 20:15:05 -0300

    Sistema sem modulo

--- src/script/app.js
+++ src/script/app.js
@@ -1,23 +1,61 @@
-import Usuario from "./usuarios.js";
-import dataUser from "./database.js";
+const cardList = document.querySelector("#card-list"); // div
 
-const cardList = document.querySelector("#card-list");
+const listaDeUsuarios = [];
 
-const listaDeUSuarios = dataUser.map((user) => {
-  return new Usuario(user.nome, user.profissao, user.img, user.descricao)
-    .criarCard;
+class Usuario {
+  constructor(nome, avatar, profissao, bio) {
+    this.nome = nome;
+    this.avatar = avatar;
+    this.profissao = profissao;
+    this.bio = bio;
+  }
+}
+let usuario1 = new Usuario(
+  "Guilherme",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "financeiro",
+  "Sou cozinheiro de profissão"
+);
+let usuario2 = new Usuario(
+  "Sabrina",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "financeiro",
+  "Aspirante a dev júnior"
+);
+let usuario3 = new Usuario(
+  "Francisco",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "jornalista",
+  "Aspirante a dev também"
+);
+
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+listaDeUsuarios.push(usuario3);
+
+class Card {
+  constructor(usuario) {
+    this.usuarioObj = usuario;
+  }
+
+  get renderizar() {
+    return `
+    <div class='card'>
+      <div class="avatar">
+        <img src="${this.usuarioObj.avatar}">
+      </div>
+      <h2>${this.usuarioObj.nome}</h2>
+      <span>${this.usuarioObj.profissao}</span>
+      <div class="card-content">
+        <h2>Bio:</h2>
+        <p>${this.usuarioObj.bio}</p>
+      </div>
+    </div>`;
+  }
+}
+
+listaDeUsuarios.forEach((usuario) => {
+  cardList.innerHTML += new Card(usuario).renderizar;
 });
 
-console.log(listaDeUSuarios);
-
-cardList.innerHTML = listaDeUSuarios.join("");
-
-// dataUser.forEach((user) => {
-//   let usuario = new Usuario(
-//     user.nome,
-//     user.profissao,
-//     user.img,
-//     user.descricao
-//   );
-//   cardList.innerHTML += usuario.criarCard;
-// });
+console.log(listaDeUsuarios);
